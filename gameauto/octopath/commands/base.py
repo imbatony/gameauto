eae83208344d939e91698cf5605a2a11e3e62231
esfak47 <esfak47@qq.com>
2024-09-22 09:25:00 +0800

    fix code

--- gameauto/octopath/commands/base.py
+++ gameauto/octopath/commands/base.py
@@ -28,23 +28,19 @@ class BaseOctopathCommand(BaseCommand):
             if "菜单" in pos.text or "商店" in pos.text or "地图" in pos.text:
                 ctx.logger.debug(f"主菜单: {pos.text}")
                 status |= OctopathStatus.Menu.value | OctopathStatus.Free.value
-                break
             if "其他" in pos.text or "道具" in pos.text or "通知" in pos.text:
                 ctx.logger.debug(f"其他菜单: {pos.text}")
                 status |= OctopathStatus.Other.value | OctopathStatus.Free.value
-                break
-            if "回合" in pos.text:
+            if "回合" in pos.text or "战斗" in pos.text:
                 ctx.logger.debug(f"战斗中: {pos.text}")
                 status |= OctopathStatus.Combat.value
-                break
-            if "战斗结算" in pos.text:
+            if "结算" in pos.text:
                 ctx.logger.debug(f"结算: {pos.text}")
                 status |= (
                     OctopathStatus.Conclusion.value
                     | OctopathStatus.Free.value
                     | OctopathStatus.Combat.value
                 )
-                break
 
         for pos in ocr_result:
             if "攻击" in pos.text and OctopathStatus.is_combat(status):
