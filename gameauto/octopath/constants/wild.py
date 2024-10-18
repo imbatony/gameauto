b7ff0ff3603aa75f5b3fcff927ef354b5a539144
esfak47 <esfak47@qq.com>
2024-10-18 14:49:50 +0800

    fix code

--- gameauto/octopath/constants/wild.py
+++ gameauto/octopath/constants/wild.py
@@ -6,6 +6,7 @@ WILD = collections.namedtuple("WILD", ["town_name_near_by", "icon_name"])
 
 wild_names: dict[str, WILD] = {
     "边狱-盖斯特峡谷": WILD("边狱-克拉古斯比亚", IconName.MAP_WILD_GORGE_HELL),
+    "边狱-艾德拉斯城": WILD("边狱-克拉古斯比亚", IconName.MAP_WILD_PALACE),
     "边狱-瓦洛雷后森": WILD("边狱-瓦洛雷", IconName.MAP_WILD_FOREST_HELL),
     "恐怖山谷": WILD("牧羊岩", IconName.MAP_WILD_GORGE),
     "静湖的地下遗址": WILD("库利亚布鲁克", IconName.MAP_WILD_RUINS),
